4bceaf3fdf415087afde9181b9fcf76b6a1c39c2
xqb64 <x@xqb.ch>
2023-11-16 05:08:21 +0100

    chore: remove some layouts (fixes #6)

--- src/layouts.rs
+++ src/layouts.rs
@@ -1,36 +1,16 @@
 use crate::{MAX_MAIN, RATIO, RATIO_STEP};
 use penrose::{
-    builtin::layout::{CenteredMain, Grid, MainAndStack, Monocle},
-    core::layout::{Layout, LayoutStack},
-    extensions::layout::{Conditional, Tatami},
+    builtin::layout::{Grid, MainAndStack, Monocle},
+    core::layout::LayoutStack,
+    extensions::layout::Tatami,
     stack,
 };
 
 pub fn layouts() -> LayoutStack {
     stack!(
-        flex_tall(),
-        flex_wide(),
         MainAndStack::side(MAX_MAIN, RATIO, RATIO_STEP),
         Tatami::boxed(RATIO, RATIO_STEP),
         Grid::boxed(),
         Monocle::boxed()
-    )
-}
-
-fn flex_tall() -> Box<dyn Layout> {
-    Conditional::boxed(
-        "FlexTall",
-        MainAndStack::side_unboxed(MAX_MAIN, RATIO, RATIO_STEP, false),
-        CenteredMain::vertical_unboxed(MAX_MAIN, RATIO, RATIO_STEP),
-        |_, r| r.w <= 1400,
-    )
-}
-
-fn flex_wide() -> Box<dyn Layout> {
-    Conditional::boxed(
-        "FlexWide",
-        MainAndStack::bottom_unboxed(MAX_MAIN, RATIO, RATIO_STEP, false),
-        CenteredMain::horizontal_unboxed(MAX_MAIN, RATIO, RATIO_STEP),
-        |_, r| r.w <= 1400,
     )
 }
